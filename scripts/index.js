fbdb0434c9b70e0662c6d7fcfad6506c7b36e955
Chris Klanac <cklanac@users.noreply.github.com>
2018-03-14 13:06:03 -0400

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+/* global $ noteful api store */
+'use strict';
+
+$(document).ready(function () {
+  noteful.bindEventListeners();
+
+  api.search('/api/notes')
+    .then(response => {
+      store.notes = response;
+      noteful.render();
+    });
+
+  console.log('Get folders, coming soon...');
+  // api.search('/api/folders')
+  //   .then(response => {
+  //     store.folders = response;
+  //     noteful.render();
+  //   });
+
+  console.log('Get tags, coming soon...');
+  // api.search('/api/tags')
+  //   .then(response => {
+  //     store.tags = response;
+  //     noteful.render();
+  //   });
+
+});
+
